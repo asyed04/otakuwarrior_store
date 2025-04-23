24fba34dd8804c2b92e88b7375f5f5f624d0a20d
asyed04 <asyed4@academic.rrc.ca>
2025-04-22 21:51:26 -0500

    Configure importmap and devise for user authentication

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap" # @5.3.4
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+
+# Use local Rails UJS instead of external CDN
+pin "@rails/ujs", to: "vendor/@rails/ujs.js"
