c83111a0db838fb643cd52af1e97089fd7bab583
asyed04 <asyed4@academic.rrc.ca>
2025-04-22 21:50:46 -0500

    Refine model associations and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,20 @@ class Product < ApplicationRecord
   has_one_attached :image
   belongs_to :category
 
+  # Validations
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, presence: true
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :image_presence
+
   # 🔽 Scopes for filtering
   scope :on_sale, -> { where(on_sale: true) }
   scope :new_products, -> { where('created_at >= ?', 3.days.ago) }
+  
+  private
+  
+  def image_presence
+    errors.add(:image, "must be attached") unless image.attached?
+  end
 end
